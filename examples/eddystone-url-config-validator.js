6ad8ca922b37ffb3971fbff7d25b2ccd60da1f18
ragingwind <ragingwind@gmail.com>
2015-08-05 16:42:17 +0900

    Renew the way of the event handling

--- examples/eddystone-url-config-validator.js
+++ examples/eddystone-url-config-validator.js
@@ -1,53 +1,33 @@
 'use strict';
 
 var beaconConfigService = require('../');
-var beaconConfig = {
-	name: 'Eddystone-URL Configuration Service',
-	lockState: false,
-	uri: 'http://google.com',
-	flags: 0,
-	txPowerLevel: 0,
-	txPowerMode: 0,
-	beaconPeriod: 0,
-	disabled: true
-};
 
-beaconConfigService.on('lockState', function (req, res) {
-	res(beaconConfigService.RESULT_SUCCESS, beaconConfig);
-	console.log('Reponse current lockstate: ', beaconConfig.lockState);
+// Change lock state to false
+beaconConfigService.unlock();
+
+beaconConfigService.on('uri', function (e) {
+	console.log('URI has been updated from %s to %s', e.prevValue, e.newValue);
 });
 
-beaconConfigService.on('uriData', function (mode, res, req) {
-	console.log('uriData', mode, res, req);
+beaconConfigService.on('flags', function (e) {
+	console.log('Flags has been updated from %s to %s', e.prevValue, e.newValue);
 });
 
-beaconConfigService.on('flags', function (mode, res, req) {
-	console.log('flags', mode, res, req);
+beaconConfigService.on('txpower-level', function (e) {
+	console.log('TX Power level has been updated from %s to %s', e.prevValue, e.newValue);
 });
 
-beaconConfigService.on('txPowerLevel', function (mode, res, req) {
-	console.log('txPowerLevel', mode, res, req);
+beaconConfigService.on('txpower-mode', function (e) {
+	console.log('TX Power mode has been updated from %s to %s', e.prevValue, e.newValue);
 });
 
-beaconConfigService.on('txPowerLevel', function (mode, res, req) {
-	console.log('txPowerLevel', mode, res, req);
+beaconConfigService.on('beacon-period', function (e) {
+	console.log('Beacone period has been updated from %s to %s', e.prevValue, e.newValue);
 });
 
-beaconConfigService.on('txPowerMode', function (mode, res, req) {
-	console.log('txPowerMode', mode, res, req);
+beaconConfigService.on('reset', function (e) {
+	console.log('Beacon should do reset');
 });
 
-beaconConfigService.on('txPowerMode', function (mode, res, req) {
-	console.log('txPowerMode', mode, res, req);
-});
-
-beaconConfigService.on('beaconPeriod', function (mode, res, req) {
-	console.log('beaconPeriod', mode, res, req);
-});
-
-beaconConfigService.on('reset', function (mode, res, req) {
-	console.log('reset', mode, res, req);
-});
-
-console.log('Eddystone beacon configuration servise has been started');
+console.log('Eddystone beacon configuration service has been started');
 beaconConfigService.advertise();
--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 'use strict';
 
 var util = require('util');
+var debug = require('debug')('es-config');
 var bleno = require('bleno');
 var objectAssign = require('object-assign');
 var Characteristic = require('bleno').Characteristic;
 var PrimaryService = bleno.PrimaryService;
 var EventEmitter = require('events').EventEmitter;
+var encoding = require('eddystone-url-encoding');
+var TxPower = require('./eddystone-tx-power');
 
-var BEACON_PERIOD_LOWEST = 10;
+function EddystoneConfigService(config) {
+	// set to initialize value
+	this._config = {};
+	this._resetConfig();
 
-function EddystoneConfigService() {
-	this.beaconConfig = {
+	// override config
+	this.configure(objectAssign({
 		name: 'Eddystone-URL Configuration Service',
-		lockState: false,
 		uri: 'http://google.com',
-		flags: 0,
-		txPowerLevel: 0,
-		txPowerMode: 0,
-		beaconPeriod: 0,
-		disabled: true
-	};
+	}, config));
 
-	this.characteristics = {
+	debug('Configuration has been updated: ');
+	debug('  name: %s', this._config.name);
+	debug('  uri: %s', this._config.uri);
+	debug('  txPower mode: %d, level: %d', this._config.txPower.mode(), this._config.txPower.level());
+	debug('  beaconPeriod: %d', this._config.beaconPeriod);
+
+	// build up beacon configure service characteristics
+	var characteristics = {
 		lockState: {
 			uuid: 'ee0c2081-8786-40ba-ab96-99b91ac981d8',
 			properties: ['read'],
@@ -71,121 +78,215 @@ function EddystoneConfigService() {
 			onWriteRequest: this._reset.bind(this)
 		}
 	};
+
+	// create beacon primaty service with characteristics
+	this.service = new PrimaryService({
+		uuid: 'ee0c2080-8786-40ba-ab96-99b91ac981d8',
+		characteristics: Object.keys(characteristics).map(function (ch) {
+			return new Characteristic(characteristics[ch]);
+		})
+	});
 }
 
 util.inherits(EddystoneConfigService, EventEmitter);
 
-EddystoneConfigService.prototype._writeBoolean = function (bool) {
-	var data = new Buffer(1);
-	data.fill(bool ? 1 : 0);
-	return data;
+function bool2buf(bool) {
+	var buf = new Buffer(1);
+	buf.fill(bool ? 1 : 0);
+	return buf;
+}
+
+function byte2buf(byte, digit) {
+	digit = digit || 1;
+
+	var write = ['writeUInt8', 'writeUInt16LE'];
+	var buf = new Buffer(digit);
+
+	buf[write[digit - 1]](byte, 0, false);
+
+	return buf;
+}
+
+EddystoneConfigService.prototype._emitWithValue = function (event, prevValue, newValue) {
+	debug('emit event with values: %s prev: %s, new: %s', event, prevValue, newValue);
+
+	this.emit(event, {prevValue: prevValue, newValue: newValue});
 };
 
 EddystoneConfigService.prototype._readLockState = function (offset, cb) {
-	this.emit('lockState', {
-		properties: ['read'],
-		offset: offset
-	}, function(err, opts) {
-		if (!opts || opts.lockState) {
-			throw new Error('Invalid lock state');
-		}
-		cb(err, this._writeBoolean(opts.lockState));
-	}.bind(this));
+	debug('reading lock state: %s', this._lockState);
+
+	cb(Characteristic.RESULT_SUCCESS, bool2buf(this._lockState));
 };
 
 EddystoneConfigService.prototype._writeUriData = function (data, offset, withoutResponse, cb) {
-	this.beaconConfig.uri = data;
-	this.emit('uriData', 'write', this.beaconConfig.uri.toString(), data);
-	cb(Characteristic.RESULT_SUCCESS);
+	debug('writing uri, lock state: %s', this._lockState);
+
+	var err = Characteristic.RESULT_UNLIKELY_ERROR;
+
+	if (this._lockState === false) {
+		var prev = this._config.uri;
+		this._config.uri = encoding.decode(data);
+		this._emitWithValue('uri', prev, this._config.uri);
+		err = Characteristic.RESULT_SUCCESS;
+	}
+
+	cb(err);
 };
 
 EddystoneConfigService.prototype._readUriData = function (offset, cb) {
-	this.emit('uriData', 'read', this.beaconConfig.uri, offset);
-	cb(Characteristic.RESULT_SUCCESS, this.beaconConfig.uri);
+	debug('Reading URI: %s, lock state: %s', this._config.uri, this._lockState);
+
+	cb(Characteristic.RESULT_SUCCESS, encoding.encode(this._config.uri));
 };
 
 EddystoneConfigService.prototype._writeFlags = function (data, offset, withoutResponse, cb) {
-	this.beaconConfig.flags = new Buffer(data);
-	this.emit('flags', 'write', this.beaconConfig.flags, data);
-	cb(Characteristic.RESULT_SUCCESS);
+	debug('Writing flags, lock state: %s', this._lockState);
+
+	var err = Characteristic.RESULT_UNLIKELY_ERROR;
+
+	if (this._lockState === false) {
+		var prev = this._config.flags;
+		this._config.flags = data.readUInt8(offset);
+		this._emitWithValue('flags', prev, this._config.flags);
+		err = Characteristic.RESULT_SUCCESS;
+	}
+
+	cb(err);
 };
 
 EddystoneConfigService.prototype._readFlags = function (offset, cb) {
-	this.emit('flags', 'read', this.beaconConfig.flags, offset);
-	cb(Characteristic.RESULT_SUCCESS, this.beaconConfig.flags);
+	debug('reading flags: %s, lock state: %s', this._config.flags, this._lockState);
+
+	cb(Characteristic.RESULT_SUCCESS, byte2buf(this._config.flags));
 };
 
 EddystoneConfigService.prototype._writeTxPowerLevel = function (data, offset, withoutResponse, cb) {
-	this.beaconConfig.txPowerLevel = new Buffer(data);
-	this.emit('txPowerLevel', 'write', this.beaconConfig.txPowerLevel, data);
-	cb(Characteristic.RESULT_SUCCESS);
+	debug('writing tx power, lock state: %s', this._lockState);
+
+	var err = Characteristic.RESULT_UNLIKELY_ERROR;
+
+	if (this._lockState === false) {
+		var prev = this._config.txPower.levels();
+		this._config.txPower.levels(data);
+		this._emitWithValue('txpower-level', prev, this._config.flags);
+		err = Characteristic.RESULT_SUCCESS;
+	}
+
+	cb(err);
 };
 
 EddystoneConfigService.prototype._readTxPowerLevel = function (offset, cb) {
-	this.emit('txPowerLevel', 'read', this.beaconConfig.txPowerLevel, offset);
-	cb(Characteristic.RESULT_SUCCESS, this.beaconConfig.txPowerLevel);
+	debug('reading flags: %s, lock state: %s', this._config.txPower.levels(), this._lockState);
+
+	cb(Characteristic.RESULT_SUCCESS, this._config.txPower.toBuffer());
 };
 
 EddystoneConfigService.prototype._writeTxPowerMode = function (data, offset, withoutResponse, cb) {
-	this.beaconConfig.txPowerMode = new Buffer(data);
-	this.emit('txPowerMode', 'write', this.beaconConfig.txPowerMode, data);
-	cb(Characteristic.RESULT_SUCCESS);
+	debug('writing tx mode, lock state: %s', this._lockState);
+
+	var err = Characteristic.RESULT_UNLIKELY_ERROR;
+
+	if (this._lockState === false) {
+		var prev = this._config.txPower.mode();
+		this._config.txPower.mode(data.readUInt8(0));
+		this._emitWithValue('txpower-mode', prev, this._config.flags);
+		err = Characteristic.RESULT_SUCCESS;
+	}
+
+	cb(err);
 };
 
 EddystoneConfigService.prototype._readTxPowerMode = function (offset, cb) {
-	this.emit('txPowerMode', 'read', this.beaconConfig.txPowerMode, offset);
-	cb(Characteristic.RESULT_SUCCESS, this.beaconConfig.txPowerMode);
+	debug('reading tx mode: %s, lock state: %s', this._config.txPower.mode(), this._lockState);
+
+	cb(Characteristic.RESULT_SUCCESS, byte2buf(this._config.txPower.mode()));
 };
 
 EddystoneConfigService.prototype._writeBeaconPeriod = function (data, offset, withoutResponse, cb) {
-	var period = data.readUInt16LE(0);
+	debug('writing becaon period, lock state: %s', this._lockState);
 
-	if (period >= BEACON_PERIOD_LOWEST) {
-		this.beaconConfig.beaconPeriod = data;
-		this.beaconConfig.disabled = false;
-	} else if (period === 0) {
-		this.beaconConfig.disabled = true;
-	} else {
-		this.beaconConfig.disabled = false;
+	var BEACON_PERIOD_LOWEST = 10;
+	var err = Characteristic.RESULT_UNLIKELY_ERROR;
+
+	if (this._lockState === false) {
+		var period = data.readUInt16LE(0);
+
+		if (period === 0) {
+			this._emitWithValue('beacon-period', 0, 0);
+			this._freezeBeacon = true;
+		} else {
+			var prev = this._config.beaconPeriod;
+			this._config.beaconPeriod = period >= BEACON_PERIOD_LOWEST ? period : this._config.beaconPeriod;
+			this._emitWithValue('beacon-period', prev, this._config.beaconPeriod);
+			this._freezeBeacon = false;
+		}
+
+		err = Characteristic.RESULT_SUCCESS;
 	}
 
-	this.emit('beaconPeriod', 'write', this.beaconConfig.beaconPeriod, data);
-	cb(Characteristic.RESULT_SUCCESS);
+	cb(err);
 };
 
 EddystoneConfigService.prototype._readBeaconPeriod = function (offset, cb) {
-	var data;
+	debug('reading becaon period: %d, freeze: %d, lock state: %s',
+				this._config.beaconPeriod,
+				this._freezeBeacon,
+				this._lockState);
 
-	if (this.beaconConfig.disabled) {
-		data = new Buffer(2);
-		data.fill(0);
-	} else {
-	 	data = new Buffer(this.beaconConfig.beaconPeriod);
-	}
-
-	this.emit('txPowerMode', 'read', this.beaconConfig.beaconPeriod, offset);
-	cb(Characteristic.RESULT_SUCCESS, data);
+	cb(Characteristic.RESULT_SUCCESS, byte2buf(this._freezeBeacon ? 0x0000 : this._config.beaconPeriod, 2));
 };
+
 
 EddystoneConfigService.prototype._reset = function (data, offset, withoutResponse, cb) {
-	console.log('reset', data);
-	this.emit('reset', 'write', data, offset);
-	cb(Characteristic.RESULT_SUCCESS);
+	var err = Characteristic.RESULT_UNLIKELY_ERROR;
+
+	if (this._lockState === false) {
+		this._resetConfig();
+		err = Characteristic.RESULT_SUCCESS;
+	}
+
+	// reset freezing status manually
+	this._freezeBeacon = false;
+
+	cb(err);
 };
 
-EddystoneConfigService.prototype._generateCharacteristics = function() {
-	var chs = this.characteristics;
-	return Object.keys(chs).map(function (ch) {
-		return new Characteristic(chs[ch]);
+EddystoneConfigService.prototype._resetConfig = function () {
+	// reset beacon config
+	this._config = objectAssign(this._config, {
+		flags: 0, // set uri flags to 0
+		txPower: new TxPower(), // set tx power mode to low
+		beaconPeriod: 1000 // beacon period to 1000 (1 second)
 	});
 };
 
+EddystoneConfigService.prototype.configure = function(config) {
+	this._config = objectAssign(this._config, config);
+
+	['name', 'uri', 'txPower', 'flags', 'beaconPeriod'].forEach(function (prop) {
+		if (this._config[prop] === undefined) {
+			throw new Error('Configs has invalid value of ', prop);
+		}
+	}.bind(this));
+
+	return this._config;
+};
+
+EddystoneConfigService.prototype.lock = function() {
+	this._lockState = true;
+};
+
+EddystoneConfigService.prototype.unlock = function() {
+	this._lockState = false;
+};
+
 EddystoneConfigService.prototype.advertise = function() {
-	var _this = this;
+	var self = this;
 
 	function start() {
 		if (bleno.state === 'poweredOn') {
-			bleno.startAdvertising(_this.beaconConfig.name, ['ee0c2080-8786-40ba-ab96-99b91ac981d8']);
+			bleno.startAdvertising(self._config.name, ['ee0c2080-8786-40ba-ab96-99b91ac981d8']);
 		} else {
 			bleno.once('stateChange', function() {
 				start();
@@ -193,32 +294,14 @@ EddystoneConfigService.prototype.advertise = function() {
 		}
 	}
 
-	// After advertising is started, set up config services
+	// after advertising is started successful, set up config services
 	bleno.once('advertisingStart', function(err) {
-		if (err) {
-			_this.emit('advertisingStart', err);
-			return;
+		if (!err) {
+	    bleno.setServices([self.service]);
 		}
-
-    bleno.setServices([
-      new PrimaryService({
-        uuid: 'ee0c2080-8786-40ba-ab96-99b91ac981d8',
-        characteristics: _this._generateCharacteristics()
-      })
-    ]);
 	});
 
 	start();
 };
-
-EddystoneConfigService.prototype.setConfig = function(opts) {
-	this.beaconConfig = objectAssign(this.beaconConfig, opts);
-};
-
-// Exports result code for convenience
-EddystoneConfigService.RESULT_SUCCESS = Characteristic.RESULT_SUCCESS;
-EddystoneConfigService.RESULT_INVALID_OFFSET = Characteristic.RESULT_INVALID_OFFSET;
-EddystoneConfigService.RESULT_INVALID_ATTRIBUTE_LENGTH = Characteristic.RESULT_INVALID_ATTRIBUTE_LENGTH;
-EddystoneConfigService.RESULT_UNLIKELY_ERROR = Characteristic.RESULT_UNLIKELY_ERROR;
 
 module.exports = new EddystoneConfigService();
