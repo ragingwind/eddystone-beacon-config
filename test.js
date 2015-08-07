f0cd99e376be995c215e4c1d206f3ce4709f49c0
ragingwind <ragingwind@gmail.com>
2015-08-07 12:13:50 +0900

    Delete test.ks

--- test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var assert = require('assert');
-var nodeEddystoneBeaconConfig = require('./');
-
-it('should ', function () {
-	assert.strictEqual(nodeEddystoneBeaconConfig('unicorns'), 'unicorns & rainbows');
-});
